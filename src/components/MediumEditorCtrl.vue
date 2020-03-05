fc43722475a96f27d734fe4d5940f31eb2895f91
lucas <lucas.f.lu@nasa.gov>
2020-03-05 15:16:43 -0800

    CK Editor

--- src/components/MediumEditorCtrl.vue
+++ src/components/MediumEditorCtrl.vue
@@ -1,151 +1,54 @@
 <template>
     <div id="FeIronEditor">
-        <imguploader :Editor="editor" />
-        <menuctrl :editor="editor" :FloatMenuActive="FloatingMenu" ref="menuCtrl"></menuctrl>
-        <editor-content :editor="editor" id="FeIronEditorArea" />
+        <ckeditor
+            id="FeIronEditorArea"
+            :editor="editor"
+            v-model="editorData"
+            :config="editorConfig"
+        ></ckeditor>
     </div>
 </template>
 
 <script>
-import { Editor, EditorContent } from "tiptap";
-import {
-    Blockquote,
-    BulletList,
-    CodeBlock,
-    HardBreak,
-    Heading,
-    ListItem,
-    OrderedList,
-    Bold,
-    Code,
-    Italic,
-    Link,
-    Placeholder,
-    HorizontalRule,
-    Underline,
-    Image,
-} from "tiptap-extensions";
-import menuctrl from "./MenuControl";
-import DropCap from "./extension/DropCap";
-import span from "./extension/span";
-import ImagePlaceholder from './extension/ImagePlaceholder';
-import imguploader from './ImageUploader';
-// import testingDoc from "./testing";
+import CKEditor from "@ckeditor/ckeditor5-vue";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+
+import EssentialsPlugin from "@ckeditor/ckeditor5-essentials/src/essentials";
+import BoldPlugin from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import ItalicPlugin from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import LinkPlugin from "@ckeditor/ckeditor5-link/src/link";
+import ParagraphPlugin from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+
 export default {
     components: {
-        EditorContent,
-        menuctrl,
-        imguploader
+        ckeditor: CKEditor.component
     },
     data() {
         return {
-            editor: new Editor({
-                autoFocus: true,
-                extensions: [
-                    new DropCap(),
-                    new span(),
-                    new Image(),
-                    new ImagePlaceholder(),
-                    // new testingDoc(),
-                    new Blockquote(),
-                    new BulletList(),
-                    new CodeBlock(),
-                    new HardBreak(),
-                    new Heading({ levels: [1, 2, 3] }),
-                    new ListItem(),
-                    new OrderedList(),
-                    new Link(),
-                    new Bold(),
-                    new Code(),
-                    new Italic(),
-                    new HorizontalRule(),
-                    new Underline(),
-                    new Placeholder({
-                        showOnlyCurrent: false,
-                        emptyNodeText: node => {
-                            if (
-                                node.type.name === "heading" 
-                                && node.attrs!==undefined
-                                && node.attrs.level==1) {
-                                return "Title ...";
-                            }
-                            return "Write something ...";
-                        }
-                    })
+            editor: ClassicEditor,
+            editorData: "<p>Content of the editor.</p>",
+            editorConfig: {
+                plugins: [
+                    EssentialsPlugin,
+                    BoldPlugin,
+                    ItalicPlugin,
+                    LinkPlugin,
+                    ParagraphPlugin
                 ],
-                onBlur: () => {
-                    this.FloatingMenu.activation = false;
-                    this.FloatingMenu.ControlTop = this.FloatingMenu.top;
-                    this.FloatingMenu.editorDom = null;
+
+                toolbar: {
+                    items: ["bold", "italic", "link", "undo", "redo"]
                 }
-            }),
-            FloatingMenu: {
-                activation: false,
-                top: 0,
-                ControlTop: 0,
-                cursorDom: null,
-                editorDom: null
             }
         };
     },
-    mounted() {
-        this.$el.addEventListener("mouseleave", () => {
-            this.FloatingMenu.activation = this.editor.focused || false;
-            if (this.editor.focused) {
-                this.FloatingMenu.top = null;
-            }
-            this.FloatingMenu.cursorDom = null;
-        });
-
-        let EditorCtrl = this.$el.querySelector("#FeIronEditorArea");
-
-        EditorCtrl.addEventListener(
-            "mouseover",
-            e => {
-                this.FloatingMenu.activation = true;
-                if (e.target.parentElement.classList.contains("ProseMirror")) {
-                    this.FloatingMenu.top = this.ComputeLocation(e.target);
-                    this.FloatingMenu.cursorDom = e.target;
-                }
-            },
-            true
-        );
-    },
-    watch: {
-        FloatingMenu: {
-            deep: true,
-            handler(newV) {
-                return newV;
-            }
-        },
-        "editor.selection.from": {
-            deep: true,
-            handler() {
-                if (this.editor.focused) {
-                    let node = this.editor.view.domAtPos(
-                        this.editor.state.selection.anchor
-                    ).node;
-                    if (node.nodeName == "#text") {
-                        node = node.parentNode;
-                    }
-                    this.FloatingMenu.top = this.FloatingMenu.ControlTop = this.ComputeLocation(
-                        node
-                    );
-                    this.FloatingMenu.editorDom = this.FloatingMenu.cursorDom = node;
-                }
-            }
-        }
-    },
-    methods: {
-        ComputeLocation(target) {
-            return target.offsetTop + target.getBoundingClientRect().height / 2;
-        }
-    },
-    beforeDestroy() {
-        this.editor.destroy();
-    }
+    mounted() {},
+    watch: {},
+    methods: {},
+    beforeDestroy() {}
 };
 </script>
+
 <style lang="scss">
 #FeIronEditor {
     * {
